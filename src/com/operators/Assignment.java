2d9a226c0d7a130300ddf32b9ed605b86c51729c
Harsha <harshavennapusa@gmail.com>
2026-08-11 13:21:44 +0530

    Assignment in java

---
+++ src/com/operators/Assignment.java
@@ -0,0 +1,25 @@
+package com.operators;
+
+public class Assignment {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Assignment Operators");
+		int a =20;
+		a +=a;
+		System.out.println(a);
+		int a1 =20;
+		a1-=a1;
+		System.out.println(a1);
+		int a2 = 20;
+		a2*=a2;
+		System.out.println(a2);
+		int a3 = 20;
+		a3%=a3;
+		System.out.println(a3);
+		int a4 = 20;
+		a4/=a4;
+		System.out.println(a4);
+
+	}
+
+}
