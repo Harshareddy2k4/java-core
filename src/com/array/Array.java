975608a29e0e3d6590dccb7fbc414aea3b51ed84
Harsha <harshavennapusa@gmail.com>
2026-08-18 19:22:34 +0530

    array

---
+++ src/com/array/Array.java
@@ -0,0 +1,15 @@
+package com.array;
+
+//initializing the array
+import java.util.Arrays;
+
+public class Array {
+
+	public static void main(String[] args) {
+		int[] arr = { 29, 34, 45, 67, 89, 90 };
+		for (int i = 0; i <= arr.length - 1; i++) {
+		}
+		System.out.println(Arrays.toString(arr));
+	}
+
+}
