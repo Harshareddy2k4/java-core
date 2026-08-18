70f03c9b02007c1c6451522dc7f78074522d22a5
Harsha <harshavennapusa13@gmail.com>
2026-08-18 19:44:27 +0530

    array practice

---
+++ src/com/array/Array18.java
@@ -0,0 +1,16 @@
+package com.array;
+
+//Find the last occurrence
+public class Array18 {
+
+	public static void main(String[] args) {
+		int[] a = { 12, 34, 56, 78, 987, 76, 1 };
+		int temp = 0;
+		for (int i = 0; i < a.length; i++) {
+			temp = a[i];
+		}
+		System.out.println("LAST OCCURANCE NUMBER :" + temp);
+
+	}
+
+}
