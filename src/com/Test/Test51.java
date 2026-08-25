6fa1f5743ade8398e830ae89e3cc56a669aad5cc
Harsha <harshavennapusa13@gmail.com>
2026-08-25 13:49:23 +0530

    arrays task

---
+++ src/com/Test/Test51.java
@@ -0,0 +1,17 @@
+package com.Test;
+
+public class Test51 {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		int[][] a = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a.length; j++) {
+				sum += a[i][j];
+			}
+		}
+		System.out.println("SUM OF NUMBERS :" + sum);
+
+	}
+
+}
