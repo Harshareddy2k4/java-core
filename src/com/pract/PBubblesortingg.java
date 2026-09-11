ef4c3c18cdc25bf38b20f49cb3464a9e7995af5f
Harsha <harshavennapusa13@gmail.com>
2026-09-11 16:04:54 +0530

    bubble sort

---
+++ src/com/pract/PBubblesortingg.java
@@ -0,0 +1,27 @@
+package com.pract;
+
+
+public class PBubblesortingg {
+
+    public static void main(String[] args) {
+
+        int[] a = {5, 3, 8, 4, 2};
+
+        for(int i = 0; i < a.length - 1; i++) {
+
+            for(int j = 0; j < a.length - 1 - i; j++) {
+
+                if(a[j] > a[j + 1]) {
+
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+
+        for(int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
