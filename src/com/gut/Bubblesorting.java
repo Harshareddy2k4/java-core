a605594312fe7edc33de98f69c3d9c22bf1a2a64
Harsha <harshavennapusa13@gmail.com>
2026-09-15 10:16:27 +0530

    bubble sorting

---
+++ src/com/gut/Bubblesorting.java
@@ -0,0 +1,21 @@
+package com.gut;
+
+public class Bubblesorting {
+
+	public static void main(String[] args) {
+		int[] a = { 5, 3, 8, 4, 2 };
+		for (int i = 0; i < a.length - 1; i++) {
+			for (int j = 0; j < a.length - 1 - i; j++) {
+				if (a[j] > a[j + 1]) {
+					int temp = a[j];
+					a[j] = a[j + 1];
+					a[j + 1] = temp;
+				}
+			}
+		}
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i]+" ");
+		}
+	}
+
+}
